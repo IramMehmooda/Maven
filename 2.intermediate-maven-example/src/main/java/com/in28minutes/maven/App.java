0354f42b7f16df8de8cef1a3c70fe2a821c3bf01
IramMehmooda <iram4011@gmail.com>
2018-03-27 18:38:38 -0500

    maven groundwork

---
+++ 2.intermediate-maven-example/src/main/java/com/in28minutes/maven/App.java
@@ -0,0 +1,17 @@
+package com.ISpring.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+	public int calculateSomething() {
+		return 0;
+	}
+}
